33f8d222002f265e432f310c3c7d9048141cace6
DylanvanHeerden <dylanvh45@gmail.com>
2022-07-28 13:32:53 +0200

    complete

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 from flask import Flask, render_template, jsonify, Response, request
+import datetime
 
 app = Flask(__name__)
 
+
+# converts the json string (time) into a python datetime object
+def convertTime(strTime):
+    format = "%Y-%m-%dT%H:%M:%SZ"
+    convertedTime = datetime.datetime.strptime(strTime, format)
+    return convertedTime
+
+
 @app.route('/')
 def index_view():
+    
     username = request.args.get('username')
-    return render_template('index.html', username = username)
+    tweets = []
+
+    
+    if(username): # if username provided
+
+        all_users = users_view().get_json() # retrieve all users (for followers data)        
+        followers = all_users[username] # create list of followers for main user
+        followers.append(username)  # add user to list of followers (used to find all tweets)) 
+        all_tweets = posts_view().get_json()  # retrieve all posts
+
+        for follower in followers: # get tweets from each follower
+            follower_tweets = all_tweets[follower]
+            for tweet in follower_tweets:
+                tweets.append([convertTime(tweet['time']),
+                              tweet['status'], follower])
+
+        tweets.sort(reverse=True) # sorts the tweets for each subarray index[0] (the date column)
+
+    return render_template('index.html', username=username, tweets=tweets)
 
 @app.route('/users')
 def users_view():
